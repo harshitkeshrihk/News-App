f9a4ea417ce7739107d51f3f2f2ef80a15695c84
harshitkeshrihk <harshitkeshrihk@gmail.com>
2022-04-09 14:50:49 +0530

    First

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants  {
+    companion object{
+        const val API_KEY = "c7630df34e66484a8c0a8a9c08f18f2a"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
